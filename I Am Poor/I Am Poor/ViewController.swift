0fc8ee83344c5e6e7c46c30a1f81be8b94684aa4
PanGeun <lifebook0809@gmail.com>
2020-06-30 22:39:12 +0900

    [add] I Am Poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 판근 on 2020/06/30.
+//  Copyright © 2020 Jeonpangeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
