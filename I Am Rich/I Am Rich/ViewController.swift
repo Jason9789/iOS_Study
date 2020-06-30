27eb6f022f8bac8fbff4cc1ba9c2e946577d889b
PanGeun <lifebook0809@gmail.com>
2020-06-30 19:15:43 +0900

    [add] I Am Rich

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 판근 on 2020/06/29.
+//  Copyright © 2020 Jeonpangeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
