4c05a69e454543a268b646421133c96eeeba9fe4
PanGeun <lifebook0809@gmail.com>
2020-07-01 15:03:14 +0900

    [add] Dice images are changed when viewDidLoad

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    // viewDidLoad란?
+    // 앱이 실행될 때 (화면이 Load가 되었을 때) 실행되는 함수.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // WHO         WHAT    VALUE
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        // Who.What = Value
+        // alpha : 이미지의 투명도를 나타내는 값.
+        // diceImageView1.alpha = 0.5
     }
 
 
