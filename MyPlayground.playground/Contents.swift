6bc9455625c523389d36e9e3bc3206bcf145fa27
PanGeun <lifebook0809@gmail.com>
2020-07-05 22:12:49 +0900

    [edit][study] switch statement

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,6 +3,18 @@
 func loveCalculator() {
     let loveScore = Int.random(in: 0...100)
     
+    switch loveScore {
+    case 81...100:
+        print("You love each other like Kanye loves Kanys.")
+    case 40..<81:
+        print("You go together like Coke and Mentos.")
+    case ..<40:
+        print("You'll be forever alone.")
+    default:
+        print("Error")
+    }
+    
+    
     if loveScore >= 80 {
         print("You love each other like Kanye loves Kanys.")
     }
