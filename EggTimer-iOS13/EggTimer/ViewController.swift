10d3a5ebe7728acc5f2c36096d556f7430a014f8
PanGeun <lifebook0809@gmail.com>
2020-07-05 21:47:38 +0900

    [add] Egg Timer with if statement

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    // 선택한 계란에 따라 soft, medium, hard 선택
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+        
+        if (hardness! == "Soft") {
+            print(softTime)
+        }
+        else if (hardness! == "Medium") {
+            print(mediumTime)
+        }
+        else if (hardness! == "Hard") {
+            print(hardTime)
+        }
+    }
+    
 }
