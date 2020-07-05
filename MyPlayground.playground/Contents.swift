5d4c500f5e6d073a503c9b93005e416ecf9a9933
PanGeun <lifebook0809@gmail.com>
2020-07-05 16:01:08 +0900

    [edit] myPlayGround for study func

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,16 +1,16 @@
+// func
 
-// Array 실습
+func greeting1() {
+    print("Hello")
+    
+    func greeting2() {
+        print("Hey")
+    }
+    
+    greeting2()
+}
 
-var numbers = [45, 73, 195, 53]
-
-var computedNumbers = [
-    numbers[0] * numbers[1],
-    numbers[1] * numbers[2],
-    numbers[2] * numbers[3],
-    numbers[3] * numbers[0]
-]
-
-print(computedNumbers)
+greeting1()
 
 
 
