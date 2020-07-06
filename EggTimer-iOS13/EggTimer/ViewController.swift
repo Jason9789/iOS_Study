8f0933b96290824bf76ca72e58b6bcae28e24bbe
PanGeun <lifebook0809@gmail.com>
2020-07-06 17:29:44 +0900

    [add] progress Bar

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,18 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLable: UILabel!
     
 //    let eggTime = ["Soft": 300, "Medium": 420, "Hard": 720]
     let eggTime = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     // time
-    var secondRemaining = 60
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    
     
 
     // 선택한 계란에 따라 soft, medium, hard 선택
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
         
         // 다른 타이머가 실행될 시 이전 타이머는 중지시키고 새로운 타이머 작동
         timer.invalidate()
@@ -30,21 +35,30 @@ class ViewController: UIViewController {
         let hardness = sender.currentTitle!
 
         // 선택된 계란에 따라서 남은 초시간 설정
-        secondRemaining = eggTime[hardness]!
+        totalTime = eggTime[hardness]!
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     // @objc : object-c
     @objc func updateTimer() {
-        if(secondRemaining > 0) {
-            print("\(secondRemaining) seconds.")
-            secondRemaining -= 1    // 1초씩 감소
+        if secondsPassed < totalTime {
+            
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            
+            progressBar.progress = Float(percentageProgress)
+            print(Float(percentageProgress))
+            
+            secondsPassed += 1    // 1초씩 감소
         }
             
         // 시간이 다 되었을 시.
         else {
             timer.invalidate()
+            
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            
+            progressBar.progress = Float(percentageProgress)
             titleLable.text = "DONE!"
             print("DONE!!")
         }
