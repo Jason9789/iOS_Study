b10965bd1214cd12c485d0d1bff3ef9d6c8f7c0a
PanGeun <lifebook0809@gmail.com>
2020-07-06 15:27:34 +0900

    [edit][study] optional

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,31 +1,14 @@
 
+//var player1Username: String = nil
 
-func loveCalculator() {
-    let loveScore = Int.random(in: 0...100)
-    
-    switch loveScore {
-    case 81...100:
-        print("You love each other like Kanye loves Kanys.")
-    case 40..<81:
-        print("You go together like Coke and Mentos.")
-    case ..<40:
-        print("You'll be forever alone.")
-    default:
-        print("Error")
-    }
-    
-    
-    if loveScore >= 80 {
-        print("You love each other like Kanye loves Kanys.")
-    }
-    else if loveScore >= 40{
-        print("You go together like Coke and Mentos.")
-    }
-        
-    else {
-        print("You'll be forever alone.")
-    }
+var play1Username: String? = nil
+
+play1Username = "pangeun"
+
+var unwrappedP1Username = play1Username!
+
+play1Username = nil
+
+if play1Username != nil {
+    print(play1Username)
 }
-
-loveCalculator()
-
