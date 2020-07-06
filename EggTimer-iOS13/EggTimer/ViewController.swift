7f4437c50101e6418895adf4e8528197d12d3d37
PanGeun <lifebook0809@gmail.com>
2020-07-06 17:04:56 +0900

    [edit] egg timer

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,30 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet weak var titleLable: UILabel!
     
+//    let eggTime = ["Soft": 300, "Medium": 420, "Hard": 720]
     let eggTime = ["Soft": 5, "Medium": 7, "Hard": 12]
     
+    // time
+    var secondRemaining = 60
+    var timer = Timer()
+    
+
     // 선택한 계란에 따라 soft, medium, hard 선택
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        // 다른 타이머가 실행될 시 이전 타이머는 중지시키고 새로운 타이머 작동
+        timer.invalidate()
+        
+        // Soft, Medium, Hard
         let hardness = sender.currentTitle!
-        let result = eggTime[hardness]!
+
+        // 선택된 계란에 따라서 남은 초시간 설정
+        secondRemaining = eggTime[hardness]!
         
-        print(result)
-        
-//        switch hardness {
-//            case "Soft":
-//                print(softTime)
-//            case "Medium":
-//                print(mediumTime)
-//            case "Hard":
-//                print(hardTime)
-//            default:
-//                print("Error")
-//        }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    // @objc : object-c
+    @objc func updateTimer() {
+        if(secondRemaining > 0) {
+            print("\(secondRemaining) seconds.")
+            secondRemaining -= 1    // 1초씩 감소
+        }
+            
+        // 시간이 다 되었을 시.
+        else {
+            timer.invalidate()
+            titleLable.text = "DONE!"
+            print("DONE!!")
+        }
     }
     
 }
