140943ba5c3c3c05e036da88895a14ed2a49b721
PanGeun <lifebook0809@gmail.com>
2020-07-07 20:32:34 +0900

    [edit][study] struct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,30 @@
 
-//var player1Username: String = nil
-
-var play1Username: String? = nil
-
-play1Username = "pangeun"
-
-var unwrappedP1Username = play1Username!
-
-play1Username = nil
-
-if play1Username != nil {
-    print(play1Username)
+// 설계도. blueprint
+struct Town {
+    let name: String
+    var citizens: [String]
+    var resources: [String: Int]
+    
+    init(name: String, citizens: [String], resources: [String: Int]) {
+        // swift에서는 this가 아니라 self
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+    
+    func fortify() {
+        print("Defences increased!")
+    }
 }
+
+var anotherTown = Town(name: "Nameless Island", citizens: ["Tom Hanks"], resources: ["Coconuts": 100])
+
+anotherTown.citizens.append("Wilson")
+
+print(anotherTown.name)
+print(anotherTown.citizens)
+
+var ghostTown = Town(name: "Ghosty McGhostface", citizens: [], resources: ["Tumbleweed": 1])
+
+anotherTown.citizens.append("Wilson")
+ghostTown.fortify()
