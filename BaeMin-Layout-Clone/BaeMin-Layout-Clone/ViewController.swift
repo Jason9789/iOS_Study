cb2335f2363f6f11350642d33427cdeecc92da30
PanGeun <lifebook0809@gmail.com>
2020-07-24 17:49:39 +0900

    [add] BaeMin Main UI Clone

---
+++ BaeMin-Layout-Clone/BaeMin-Layout-Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaeMin-Layout-Clone
+//
+//  Created by 판근 on 2020/07/24.
+//  Copyright © 2020 Jeonpangeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
