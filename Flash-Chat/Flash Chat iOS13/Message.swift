8bf4c92554528b08395c9abd72188a400361414a
PanGeun <lifebook0809@gmail.com>
2020-07-31 20:10:40 +0900

    [add] user a UITableVIew and Create a Message Model

---
+++ Flash-Chat/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 판근 on 2020/07/31.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // ex) email
+    let body: String
+}
