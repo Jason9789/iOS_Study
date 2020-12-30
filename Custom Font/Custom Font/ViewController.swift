08c12a606ca7838338e40d40a7a658d3a3f24322
Jason9789 <lifebook0809@gmail.com>
2020-12-31 01:47:32 +0900

    [ADD] 코드로 View 만들어보는 연습

--- Custom Font/Custom Font/ViewController.swift
+++ Custom Font/Custom Font/ViewController.swift
@@ -9,11 +9,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let fonts = ["kefa", "papyrus", "zapfino", "bradleyHand"]
+    
+    let customTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.layer.borderWidth = 1.0
+        textView.layer.borderColor = UIColor.black.cgColor
+        textView.textAlignment = .center
+        
+        return textView
+    }()
+    
+    let changeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Change Font", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.setTitleColor(UIColor.white, for: .highlighted)
+        button.addTarget(self, action: #selector(changeButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "Custom Font"
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(customTextView)
+        view.addSubview(changeButton)
+        
+        setCustomTextView(customTextView)
+        setChangeButton(changeButton)
+        
+    }
+    
+    
+    @objc
+    func changeButtonTapped(sender: UIButton) {
+        if let font = fonts.randomElement() {
+            customTextView.font = UIFont.init(name: font, size: 25)
+        }
+    }
+    
+    func setCustomTextView(_ textView: UITextView) {
+        NSLayoutConstraint.activate([
+            textView.widthAnchor.constraint(equalToConstant: 100),
+            textView.heightAnchor.constraint(equalToConstant: 100),
+            textView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            textView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 50),
+            textView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -50),
+        ])
     }
 
+    
+    func setChangeButton(_ button: UIButton) {
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.topAnchor.constraint(equalTo: customTextView.bottomAnchor, constant: 50)
+        ])
+    }
 
 }
 
+
+extension UIViewController: UITextViewDelegate {
+    
+    public func textViewDidChange(_ textView: UITextView) {
+        let size = CGSize(width: textView.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        
+        textView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
+    }
+}
