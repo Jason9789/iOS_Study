df3edfb337d85ff795a876dcfe4c2eddb2e384c3
Jason9789 <lifebook0809@gmail.com>
2021-01-05 01:04:48 +0900

    [ADD] Custom TabBar 프로젝트 뼈대

---
+++ Chaper03-TabBar/Chaper03-TabBar/ThirdViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdViewController.swift
+//  Chaper03-TabBar
+//
+//  Created by 전판근 on 2021/01/05.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    override func viewDidLoad() {
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        title.text = "세 번째 탭"
+        title.textColor = UIColor.red
+        title.textAlignment = .center
+        title.font = UIFont.boldSystemFont(ofSize: 13)
+        title.sizeToFit()   // 콘텐츠의 내용에 맞게 레이블 크기 변경
+        title.center.x = self.view.frame.width / 2
+        
+        self.view.addSubview(title)
+    }
+}
