a0fcf77ae2308db804845725e757cead5a756fa6
Jason9789 <lifebook0809@gmail.com>
2021-01-04 22:01:44 +0900

    [ADD] VC의 값을 RVC로 넘기기

--- Chapter02-InputForm/Chapter02-InputForm/ViewController.swift
+++ Chapter02-InputForm/Chapter02-InputForm/ViewController.swift
@@ -88,6 +88,10 @@ class ViewController: UIViewController {
         
         self.paramUpdate.addTarget(self, action: #selector(presentUpdateValue(_:)), for: .valueChanged)
         self.paramInterval.addTarget(self, action: #selector(presentIntervalValue(_:)), for: .valueChanged)
+        
+        // 전송 버튼을 내비게이션 아이템에 추가하고, submit 메소드에 연결한다.
+        let submitBtn = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(submit(_:)))
+        self.navigationItem.rightBarButtonItem = submitBtn
     }
     
     
@@ -97,6 +101,15 @@ class ViewController: UIViewController {
     
     @objc func presentIntervalValue(_ sender: UIStepper) {
         self.txtInterval.text = ("\( Int(sender.value) )분마다")
+    }
+    
+    @objc func submit(_ sender: Any) {
+        let rvc = ReadViewController()
+        rvc.pEmail = self.paramEmail.text
+        rvc.pUpdate = self.paramUpdate.isOn
+        rvc.pInterval = self.paramInterval.value
+        
+        self.navigationController?.pushViewController(rvc, animated: true)
     }
 }
 
