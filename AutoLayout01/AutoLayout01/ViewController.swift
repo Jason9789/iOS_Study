53b3a524bacb24582bf370b20c1371a6b0e0fddf
Jason9789 <lifebook0809@gmail.com>
2021-01-10 02:06:10 +0900

    [MODIFY] Autolayout 해상도 대응

--- AutoLayout01/AutoLayout01/ViewController.swift
+++ AutoLayout01/AutoLayout01/ViewController.swift
@@ -33,59 +33,28 @@ class ViewController: UIViewController {
     
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        view.addSubview(redView)
-        view.addSubview(greenView)
-        view.addSubview(blueView)
-        
-        redView.snp.makeConstraints { (make) in
-            make.width.height.equalTo(100)
-            make.left.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide)
+            super.viewDidLoad()
+            view.backgroundColor = .white
+            view.addSubview(redView)
+            view.addSubview(greenView)
+            view.addSubview(blueView)
+            
+            redView.snp.makeConstraints { (make) in
+                make.width.height.equalTo(100 * view.frame.width / 320)
+                make.left.equalToSuperview()
+                make.top.equalTo(view.safeAreaLayoutGuide)
+            }
+            
+            greenView.snp.makeConstraints { (make) in
+                make.width.height.equalTo(100 * view.frame.width / 320)
+                make.top.equalTo(view.safeAreaLayoutGuide)
+                make.left.equalTo(redView.snp.right).offset(10 * view.frame.width / 320)
+            }
+            
+            blueView.snp.makeConstraints { (make) in
+                make.width.height.equalTo(100 * view.frame.width / 320)
+                make.top.equalTo(view.safeAreaLayoutGuide)
+                make.left.equalTo(greenView.snp.right).offset(10 * view.frame.width / 320)
+            }
         }
-        
-        greenView.snp.makeConstraints { (make) in
-            make.width.height.equalTo(100)
-            make.top.equalTo(view.safeAreaLayoutGuide)
-            make.left.equalTo(redView.snp.right).offset(10)
-        }
-        
-        blueView.snp.makeConstraints { (make) in
-            make.width.height.equalTo(100)
-            make.top.equalTo(view.safeAreaLayoutGuide)
-            make.left.equalTo(greenView.snp.right).offset(10)
-        }
-    }
 }
-//
-//class ViewController: UIViewController {
-//
-//    let nameLabel = UILabel()
-//    let nameTextField = UITextField()
-//    let changeButton = UIButton()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        self.view.backgroundColor = UIColor.white
-//        self.view.addSubview(nameLabel)
-//        self.view.addSubview(nameTextField)
-//        self.view.addSubview(changeButton)
-//
-//        self.nameLabel.snp.makeConstraints {
-//            $0.top.equalToSuperview().offset(80)
-//            $0.leading.equalToSuperview().offset(24)
-//            $0.trailing.equalToSuperview().offset(-24)
-//        }
-//        self.nameTextField.snp.makeConstraints{
-//            $0.top.equalTo(self.nameLabel.snp.bottom).offset(24)
-//            $0.leading.equalTo(self.nameLabel)
-//            $0.trailing.equalTo(self.nameLabel)
-//        }
-//        self.changeButton.snp.makeConstraints {
-//            $0.center.equalToSuperview()
-//        }
-//    }
-//}
-
