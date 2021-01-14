50045c8f261844914b87be23e7b42660c1e4eba5
Jason9789 <lifebook0809@gmail.com>
2021-01-14 15:50:55 +0900

    [MODIFY] ADD VC 수정

--- ToDoLIst/ToDoLIst/ViewController.swift
+++ ToDoLIst/ToDoLIst/ViewController.swift
@@ -11,15 +11,31 @@ class ViewController: UIViewController {
 
     @IBAction func addButton(_ sender: Any) {
         let addVC = self.storyboard?.instantiateViewController(withIdentifier: "AddViewController")
-//        addVC?.modalPresentationStyle = .fullScreen
-//        self.present(addVC!, animated: true, completion: nil)
         self.navigationController?.pushViewController(addVC!, animated: true)
     }
+    
+    @IBOutlet weak var todoListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        todoListTableView?.delegate = self
+        todoListTableView?.dataSource = self
     }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath)"
+        
+        return cell
+    }
+    
+    
 }
 
