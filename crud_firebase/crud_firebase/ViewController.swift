481244ef3b92fb1c15d982517bc69eadb3e03259
Jason9789 <lifebook0809@gmail.com>
2021-01-21 23:59:15 +0900

    [ADD] Login 기능

--- crud_firebase/crud_firebase/ViewController.swift
+++ crud_firebase/crud_firebase/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let user = Auth.auth().currentUser {
+            emailTextField.placeholder = "이미 로그인 된 상태입니다."
+            passwordTextField.placeholder = "이미 로그인 된 상태입니다."
+            LoginButton.setTitle("이미 로그인 된 상태입니다.", for: .normal)
+        }
     }
 
-
+    @IBAction func loginTouchButton(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { user, error in
+            if user != nil {
+                print("Login success")
+            } else {
+                print("Login fail")
+            }
+        }
+    }
+    
 }
 
