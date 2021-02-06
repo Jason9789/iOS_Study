c997deed2b8c3006de83d4820657d60ebf7f53b3
Jason9789 <lifebook0809@gmail.com>
2021-02-07 00:12:57 +0900

    [MODIFY] String Extension 사용하여 영상 ID 추출

--- youtubePlayer/youtubePlayer/ViewController.swift
+++ youtubePlayer/youtubePlayer/ViewController.swift
@@ -12,11 +12,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var playerView: WKYTPlayerView!
     let playVarsDic = ["playsinline": 1]
+    let url = "https://www.youtube.com/watch?v=iimVJFovKps"
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        playerView.load(withVideoId: "iimVJFovKps", playerVars: playVarsDic)
+//        playerView.load(withVideoId: "iimVJFovKps", playerVars: playVarsDic)
+        if let id = url.youTubeId {
+            playerView.load(withVideoId: id, playerVars: playVarsDic)
+        }
     }
 }
 
+extension String {
+    var youTubeId: String? {
+        let typePattern = "(?:(?:\\.be\\/|embed\\/|v\\/|\\?v=|\\&v=|\\/videos\\/)|(?:[\\w+]+#\\w\\/\\w(?:\\/[\\w]+)?\\/\\w\\/))([\\w-_]+)"
+        let regex = try? NSRegularExpression(pattern: typePattern, options: .caseInsensitive)
+        return regex
+            .flatMap { $0.firstMatch(in: self, range: NSMakeRange(0, self.count)) }
+            .flatMap { Range($0.range(at: 1), in: self) }
+            .map { String(self[$0]) }
+    }
+}
