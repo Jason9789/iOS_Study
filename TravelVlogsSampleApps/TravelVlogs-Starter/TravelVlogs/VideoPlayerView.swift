42a62ba0eaaf23cd930e89b89ccf6b02a424b37f
Jason9789 <lifebook0809@gmail.com>
2021-02-09 21:15:13 +0900

    [ADD] Writing a Custom Video View with AVPlayerLayer

--- TravelVlogsSampleApps/TravelVlogs-Starter/TravelVlogs/VideoPlayerView.swift
+++ TravelVlogsSampleApps/TravelVlogs-Starter/TravelVlogs/VideoPlayerView.swift
@@ -29,7 +29,25 @@
  */
 
 import UIKit
+import AVFoundation
 
 class VideoPlayerView: UIView {
-  
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
 }
