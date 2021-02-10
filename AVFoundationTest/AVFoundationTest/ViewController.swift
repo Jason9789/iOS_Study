e89b878671dbae57e4fbf50ec682b5e40ef63fd2
Jason9789 <lifebook0809@gmail.com>
2021-02-10 23:44:21 +0900

    [ADD] Project init

---
+++ AVFoundationTest/AVFoundationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVFoundationTest
+//
+//  Created by 전판근 on 2021/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
