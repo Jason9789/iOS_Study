491be18395bb9ecadc13e173cb294a5924628bef
Jason9789 <lifebook0809@gmail.com>
2021-02-12 00:03:27 +0900

    [ADD] Video 처리

--- AVKitTest/AVKitTest/ViewController.swift
+++ AVKitTest/AVKitTest/ViewController.swift
@@ -10,7 +10,7 @@ import AVKit
 import MobileCoreServices
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,7 +29,27 @@ class ViewController: UIViewController {
 
 
 extension ViewController: UIImagePickerControllerDelegate {
-    
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+    ) {
+        // 1
+        guard let mediaType = info[UIImagePickerController.InfoKey.mediaType] as? String,
+              mediaType == (kUTTypeMovie as String),
+              let url = info[UIImagePickerController.InfoKey.mediaURL] as? URL
+        else {
+            return
+        }
+        
+        // 2
+        dismiss(animated: true) {
+            // 3
+            let player = AVPlayer(url: url)
+            let vcPlayer = AVPlayerViewController()
+            vcPlayer.player = player
+            self.present(vcPlayer, animated: true, completion: nil)
+        }
+    }
 }
 
 extension ViewController: UINavigationControllerDelegate {
