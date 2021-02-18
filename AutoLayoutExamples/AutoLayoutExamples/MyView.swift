8975d0c8567f39cfcb493f3c64b00cb297b397bf
Jason9789 <lifebook0809@gmail.com>
2021-02-18 19:26:49 +0900

    [MODIFY] Intrinsic Content Size

---
+++ AutoLayoutExamples/AutoLayoutExamples/MyView.swift
@@ -0,0 +1,17 @@
+//
+//  MyView.swift
+//  AutoLayoutExamples
+//
+//  Created by 전판근 on 2021/02/18.
+//
+
+import UIKit
+
+@IBDesignable
+class MyView: UIView {
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+
+}
