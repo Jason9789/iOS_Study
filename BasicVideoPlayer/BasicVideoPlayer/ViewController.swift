e70578744eddf281fabd49ee526069d366bec8a7
Jason9789 <lifebook0809@gmail.com>
2021-02-23 00:52:31 +0900

    임시 커밋

--- BasicVideoPlayer/BasicVideoPlayer/ViewController.swift
+++ BasicVideoPlayer/BasicVideoPlayer/ViewController.swift
@@ -13,20 +13,21 @@ class ViewController: UIViewController {
     
     @IBAction func playVideo(_ sender: Any) {
         
-        guard let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else {
-                return
-            }
-            // Create an AVPlayer, passing it the HTTP Live Streaming URL.
-            let player = AVPlayer(url: url)
-
-            // Create a new AVPlayerViewController and pass it a reference to the player.
-            let controller = AVPlayerViewController()
-            controller.player = player
-
-            // Modally present the player and call the player's play() method when complete.
-            present(controller, animated: true) {
-                player.play()
-            }
+        print("Touch Play Video Button")
+//        guard let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else {
+//                return
+//            }
+//            // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+//            let player = AVPlayer(url: url)
+//
+//            // Create a new AVPlayerViewController and pass it a reference to the player.
+//            let controller = AVPlayerViewController()
+//            controller.player = player
+//
+//            // Modally present the player and call the player's play() method when complete.
+//            present(controller, animated: true) {
+//                player.play()
+//            }
     }
     
     var video1: String?
