02299cd42b8cb5713315236c96c0a92fdd95ee73
Jason9789 <lifebook0809@gmail.com>
2021-02-25 18:01:32 +0900

    [INIT] 카카오 로그인 테스트 프로젝트 init

---
+++ kakaoLoginTest/kakaoLoginTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kakaoLoginTest
+//
+//  Created by 전판근 on 2021/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
