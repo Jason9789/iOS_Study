e84bd09be3329d1f26eaf7966c1aec1d14ac6a20
Jason9789 <lifebook0809@gmail.com>
2021-03-06 14:32:07 +0900

    [ADD] Profile View With StackView

---
+++ stackViewTest-01/stackViewTest-01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stackViewTest-01
+//
+//  Created by 전판근 on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
