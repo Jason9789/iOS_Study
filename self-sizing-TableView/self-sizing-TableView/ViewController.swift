7639a7d62ec297703bd04702554ccb008ffe4953
Jason9789 <lifebook0809@gmail.com>
2021-03-13 00:09:22 +0900

    [ADD] TableView

--- self-sizing-TableView/self-sizing-TableView/ViewController.swift
+++ self-sizing-TableView/self-sizing-TableView/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = indexPath.description
+        cell.detailTextLabel?.text = """
+            asdfasdfqwefijaowsdikjfl;askd
+            asdikfjoqiwejfoasdlkfjaszlkdf
+            asdoikfjo;qiwejf
+            asldkjfoqiwjefoaisjdl;k
+            """
+        cell.detailTextLabel?.numberOfLines = 0
+        cell.imageView?.image = UIImage(named: "\(indexPath.row % 3)")
+        
+        return cell
+    }
 }
 
