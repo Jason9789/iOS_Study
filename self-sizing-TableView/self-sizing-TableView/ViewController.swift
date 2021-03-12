cbf39a137f0e39213e49e3d6dcea91c12ff9118e
Jason9789 <lifebook0809@gmail.com>
2021-03-12 23:37:20 +0900

    [ADD] Project init

---
+++ self-sizing-TableView/self-sizing-TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  self-sizing-TableView
+//
+//  Created by 전판근 on 2021/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
