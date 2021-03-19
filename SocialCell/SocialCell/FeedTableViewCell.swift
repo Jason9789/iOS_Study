55de511d432b2eb40c9bc8e88f6cef636ed74f55
Jason9789 <lifebook0809@gmail.com>
2021-03-19 14:44:59 +0900

    [ADD] Social Media Profile init

---
+++ SocialCell/SocialCell/FeedTableViewCell.swift
@@ -0,0 +1,133 @@
+//
+//  SocialCell - FeedTableViewCell.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    
+    var feed: Feed! {
+        didSet {
+            profileImageView?.image = feed.author.profileImage
+            authorLabel?.text = feed.author.name
+            timeLabel?.text = feed.time
+            contentTextLabel?.text = feed.contents.text
+            contentImageView?.image = feed.contents.image
+            likesLabel?.text = "\(feed.likes)"
+        }
+    }
+    
+    private var profileImageView: UIImageView!
+    private var authorLabel: UILabel!
+    private var timeLabel: UILabel!
+    private var contentTextLabel: UILabel!
+    private var contentImageView: UIImageView!
+    private var likesLabel: UILabel!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        
+        super.init(style: style,
+                   reuseIdentifier: reuseIdentifier)
+        
+        drawCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+extension FeedTableViewCell {
+    
+    private func drawCell() {
+        selectionStyle = .none
+        
+        profileImageView = UIImageView(image: UIImage(systemName: "person.fill"))
+        profileImageView.clipsToBounds = true
+        profileImageView.contentMode = .scaleAspectFit
+        
+        authorLabel = UILabel()
+        authorLabel.adjustsFontForContentSizeCategory = true
+        authorLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        authorLabel.textColor = .black
+        authorLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        authorLabel.setContentCompressionResistancePriority(.required, for: .vertical)
+        authorLabel.text = "팡근"
+        
+        timeLabel = UILabel()
+        timeLabel.adjustsFontForContentSizeCategory = true
+        timeLabel.font = UIFont.preferredFont(forTextStyle: .caption2)
+        timeLabel.textColor = .darkGray
+        timeLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        timeLabel.setContentCompressionResistancePriority(.required, for: .vertical)
+        timeLabel.text = "1분 전"
+        
+        let profileStack = UIStackView(arrangedSubviews: [profileImageView, authorLabel, timeLabel])
+        profileStack.axis = .horizontal
+        profileStack.spacing = UIStackView.spacingUseSystem
+        
+        contentTextLabel = UILabel()
+        contentTextLabel.adjustsFontForContentSizeCategory = true
+        contentTextLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        contentTextLabel.textColor = .black
+        contentTextLabel.numberOfLines = 0
+        contentTextLabel.text = """
+            더미 텍스트
+            여러줄을
+            넣어봅시다
+            """
+        
+        contentImageView = UIImageView(image: UIImage(systemName: "photo"))
+        contentImageView.clipsToBounds = true
+        contentImageView.contentMode = .scaleAspectFill
+        contentImageView.isUserInteractionEnabled = true
+        
+        let likesImageView = UIImageView(image: UIImage(systemName: "hand.thumbsup.fill"))
+        likesImageView.tintColor = .systemBlue
+        likesImageView.contentMode = .scaleAspectFit
+        likesImageView.setContentHuggingPriority(.required, for: .horizontal)
+        
+        likesLabel = UILabel()
+        likesLabel.adjustsFontForContentSizeCategory = true
+        likesLabel.font = UIFont.preferredFont(forTextStyle: .callout)
+        likesLabel.textColor = .darkGray
+        likesLabel.text = "999"
+        
+        let likesStack = UIStackView(arrangedSubviews: [likesImageView, likesLabel])
+        likesStack.alignment = .center
+        likesStack.distribution = .fill
+        likesStack.axis = .horizontal
+        likesStack.spacing = UIStackView.spacingUseSystem
+        
+        let buttonStack = UIStackView()
+        buttonStack.axis = .horizontal
+        buttonStack.distribution = .fillEqually
+        buttonStack.alignment = .center
+        
+        ["좋아요", "댓글 달기", "공유하기"].forEach{ (title) in
+            let button = UIButton(type: .system)
+            button.setTitle(title, for: .normal)
+            button.tintColor = .darkGray
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.gray.cgColor
+            buttonStack.addArrangedSubview(button)
+        }
+        
+        let contentStack = UIStackView(arrangedSubviews: [profileStack, contentTextLabel, contentImageView, likesStack, buttonStack])
+        
+        contentStack.translatesAutoresizingMaskIntoConstraints = false
+        contentStack.axis = .vertical
+        contentStack.alignment = .fill
+        contentStack.distribution = .fill
+        contentStack.spacing = UIStackView.spacingUseSystem
+        contentView.addSubview(contentStack)
+        
+        
+    }
+    
+    @objc private func tapImageView(_ sender: UITapGestureRecognizer) {
+        
+    }
+}
