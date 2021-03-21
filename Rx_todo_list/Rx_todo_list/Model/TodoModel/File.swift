5e3b84cf2ee483f79f66517d3f81818e83c3f6c6
Jason9789 <lifebook0809@gmail.com>
2021-03-21 23:55:37 +0900

    [MODIFY] Model 수정

--- Rx_todo_list/Rx_todo_list/Model/TodoModel/File.swift
+++ Rx_todo_list/Rx_todo_list/Model/TodoModel/File.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+
+struct Todo {
+    let title: String
+}
--- Rx_todo_list/Rx_todo_list/View/ViewController.swift
+++ Rx_todo_list/Rx_todo_list/View/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
 
 
+extension ViewController: UITableViewDataSource,UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath)"
+        cell.detailTextLabel?.text = "Hi"
+        return cell
+    }
 }
 
